1a1669cedaebe8c10e2c27d49d821050a8bf3b8d
Adam Liliemark <dev@liliemark.se>
2026-01-20 12:18:55 +0100

    article - one year elixir started

--- lib/mix/tasks/post.ex
+++ lib/mix/tasks/post.ex
@@ -2,24 +2,27 @@ defmodule Mix.Tasks.Ack.Post do
   use Mix.Task
 
   def run([file_name, keywords]) do
-    app_dir = File.cwd!
+    app_dir = File.cwd!()
 
     current_datetime_utc = DateTime.utc_now()
-    {:ok, current_date} = Date.new(current_datetime_utc.year, current_datetime_utc.month, current_datetime_utc.day)
+
+    {:ok, current_date} =
+      Date.new(current_datetime_utc.year, current_datetime_utc.month, current_datetime_utc.day)
+
     date = current_date |> Date.to_string()
 
-    id = file_name
+    id =
+      file_name
       |> String.downcase()
       |> String.replace(~r/[^a-z]+/, "-")
       |> String.trim("-")
 
-    new_file_path = Path.join(
-        [app_dir, "pages", "posts", "#{date}-#{id}.md"]
-    )
+    new_file_path = Path.join([app_dir, "pages", "posts", "#{date}-#{id}.dj"])
 
-    keywords = keywords
+    keywords =
+      keywords
       |> String.split(" ")
-      |> Enum.map(&("\"#{&1}\""))
+      |> Enum.map(&"\"#{&1}\"")
       |> Enum.join(", ")
 
     File.write(
