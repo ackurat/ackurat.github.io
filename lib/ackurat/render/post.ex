6d1b9902868f2b390684f91432ad9f41e873fbc6
Adam Liliemark <dev@liliemark.se>
2026-01-25 14:00:46 +0100

    fix: aria labels on post nav

--- lib/ackurat/render/post.ex
+++ lib/ackurat/render/post.ex
@@ -36,8 +36,8 @@ defmodule Ackurat.Render.Post do
         <nav class="sticky top-4">
           <ol class="list-none space-y-2 text-sm pl-0">
             <li class="ml-0">
-                <a href="#top">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                <a href="#top" alt="Scroll to top" aria-label="Scroll to top">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" aria-label="Icon showing an arrow pointing upwards, enclosed in a circle">
                     <!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE -->
                     <path fill="currentColor" d="M11 16h2v-4.2l1.6 1.6L16 12l-4-4l-4 4l1.4 1.4l1.6-1.6zm1 6q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22"/>
                   </svg>
